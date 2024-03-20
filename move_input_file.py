116d91634ba48444068cda8f20ca93182a5ee16e
Techna <104751656+technahuynh@users.noreply.github.com>
2024-03-20 16:27:53 +0100

    It works T-T

--- move_input_file.py
+++ move_input_file.py
@@ -7,11 +7,12 @@ def main(argv):
     Args:
         argv (list[type]]): Provided CLI arguments.
     """
-    input_file = argv[1] # Assign CLI arguments
+    input_file, output_file = argv[1:3] # Assign CLI arguments
     print(f'Input file: {input_file}')
+    print(f'Output file: {output_file}')
     with open(input_file, 'r') as f:
         data = f.read()
-        with open('data/input.json', 'w') as f:
+        with open(output_file, 'w') as f:
             f.write(data)
 
 if __name__ == "__main__":
